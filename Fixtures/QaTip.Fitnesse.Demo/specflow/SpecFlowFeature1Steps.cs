38c86a2aed2ace763a8be0bd26513490c7dfcb61
Mangal <es1vx5i@na.corp.mckesson.com>
2016-07-21 12:29:01 -0400

    Commiting Enhancement to webservice testing class as well as adding support for Specflow when testing BDD

---
+++ Fixtures/QaTip.Fitnesse.Demo/specflow/SpecFlowFeature1Steps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace specflow
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            int P1 = 0;
+            ScenarioContext.Current.Pending();
+            p0 = p0 + P1;
+        }
+    }
+}
